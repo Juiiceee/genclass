d2b82127155e66500ba63270306e81275cfa246d
Juiceee <lbehr@student.42.fr>
2024-05-15 12:00:43 +0200

    oui

---
+++ bash.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+echo "Enter the file name:"
+read fileName
+
+echo "Creating ${fileName}.hpp..."
+
+echo "Enter the class name:"
+read className
+
+filemaj=$(echo "$fileName" | tr '[:lower:]' '[:upper:]')
+cat >> ${fileName}.hpp << EOF
+#ifndef ${filemaj}.HPP
+# define ${filemaj}.HPP
+# include <iostream>
+# include <string>
+EOF
+cat >> ${fileName}.hpp << EOF
+
+class $className
+{
+	public:
+		$className();
+		$className(const $className &obj);
+		$className &operator=(const $className &obj);
+		~$className();
+};
+EOF
+cat >> ${fileName}.hpp << EOF
+#endif
+EOF
+echo "Creating ${fileName}.cpp..."
+
+cat > ${fileName}.cpp << EOF
+#include "${fileName}.hpp"
+
+$className::$className()
+{
+
+}
+
+$className::$className(const $className &obj)
+{
+	
+}
+
+$className &$className::operator=(const $className &obj)
+{
+	if (this != &obj) {
+	}
+	return *this;
+}
+
+$className::~$className()
+{
+	
+}
+EOF
+
+echo "File ${className}.cpp has been created."
